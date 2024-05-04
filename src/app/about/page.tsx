41ade3501be829d802dd60e58fc9a90ade8e3e56
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-05-04 20:46:59 +0800

    added prisma and supabase database

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+  return (
+    <div className="h-screen max-w-screen-xl mx-auto flex justify-center items-center p-4">
+        <h1 className="text-3xl">About page</h1>
+    </div>
+  )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function page() {
+    return (
+      <div className="h-screen max-w-screen-xl mx-auto flex justify-center items-center p-4">
+          <h1 className="text-3xl">Contact page</h1>
+      </div>
+    )
+  }
+  
